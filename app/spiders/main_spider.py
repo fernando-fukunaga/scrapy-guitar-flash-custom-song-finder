87c8e1dccfcc138bfbc9dc1f0960ed74f458ca4d
fernando-fukunaga <fernandofukunaga14@gmail.com>
2025-07-19 14:54:44 -0300

    feat: improving the spider for not parsing repeated songs on the output

--- app/spiders/main_spider.py
+++ app/spiders/main_spider.py
@@ -11,16 +11,26 @@ def generate_url_list_for_song_pages(pages: int):
 class MainSpider(Spider):
     name = "guitarflashcrawler"
     allowed_domains = ["guitarflash.com"]
-    start_urls = list(generate_url_list_for_song_pages(85))
+    start_urls = list(generate_url_list_for_song_pages(86))
 
     def __init__(self, search_string: str, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.search_string = search_string
+        self.already_parsed_songs = set()
 
     def parse(self, response, **kwargs):
         for table_row in response.css("tr")[1:]:
-            if self.search_string in table_row.css("td a::text").get():
+            song_name = table_row.css("td a::text").get()
+            song_url = table_row.css("td a").xpath("@href").get()
+
+            if (
+                song_name
+                and self.search_string.lower() in song_name.lower()
+                and song_url
+                and song_url not in self.already_parsed_songs
+            ):
+                self.already_parsed_songs.add(song_url)
                 yield {
-                    "song_name": table_row.css("td a::text").get(),
-                    "song_url": table_row.css("td a").xpath("@href").get()
+                    "song_name": song_name,
+                    "song_url": song_url
                 }
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 Running the app:
 ```commandline
-scrapy crawl guitarflashcrawler -o output.json -a search_string=SONG_OR_BAND_YOU_WANNA_SEARCH_FOR
+scrapy crawl guitarflashcrawler -o output.json -a search_string="SONG_OR_BAND_YOU_WANNA_SEARCH_FOR"
 ```
+You can run this command either on the root of the project or the /app subdirectory.
